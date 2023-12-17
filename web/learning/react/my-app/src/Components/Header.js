09cdbe8b95139c5f6133600d01d660eaea1e91f6
sayanbose-0000 <bosesayan0000@gmail.com>
2023-12-17 09:10:08 +0530

    added stuffs

--- web/learning/react/my-app/src/Components/Header.js
+++ web/learning/react/my-app/src/Components/Header.js
@@ -1,12 +1,50 @@
 import React from "react";
 
 const Header = () => {
+    const [items, setItems] = React.useState([
+        {
+            id: 1,
+            checked: false,
+            item: "Potato",
+        },
+        {
+            id: 2,
+            checked: false,
+            item: "Tomato",
+        },
+        {
+            id: 3,
+            checked: false,
+            item: "Cauliflower",
+        },
+    ])
+
+    const handleChecked = (id) => {
+        const listItems = items.map(item => item.id === id ? { ...item, checked: !item.checked } : item);
+        setItems(listItems);
+    };
+
+    const handleDelete = (id) => {
+        const listItems = items.filter(item => item.id !== id);
+        setItems(listItems);
+    };
 
     return (
         <header>
             <h1 className="heading">Groceries List</h1>
+            <ul>
+                {items.map((item) => (
+                    <li className="item" key={item.id}>
+                        <input type="checkbox" checked={item.checked} onChange={() => { handleChecked(item.id) }} />
+                        <label htmlFor="" style={item.checked ? { textDecoration: 'line-through' } : null}>{item.item}</label>
+                        <button onClick={() => { handleDelete(item.id) }}>Del</button>
+                    </li>
+                ))}
+            </ul>
         </header>
     );
 };
 
 export default Header;
+
+
