335b839c4815c736754db07f2c45c09abf05958c
sayanbose-0000 <bosesayan0000@gmail.com>
2023-12-18 17:59:53 +0530

    added contents

---
+++ web/learning/react/my-app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Content from './Components/Content';
+
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Content></Content>
+    </>
+  )
+}
+
+export default App;
---
+++ web/learning/react/my-app/src/Components/Content.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Content=()=>{
+  return (
+    <p> Hello World </p>  
+  );
+}
+
+export default Content;
