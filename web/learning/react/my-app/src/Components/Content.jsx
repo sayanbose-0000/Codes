91125e73936b87ee2a5d586e0fe21c36d32c1b45
“sayanbose-0000 <bosesayan0000@gmail.com>
2023-12-18 20:33:30 +0530

    added app named text_utils

--- web/learning/react/my-app/src/Components/Content.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Content=()=>{
-  return (
-    <p> Hello World </p>  
-  );
-}
-
-export default Content;
---
+++ web/project/text_utils/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+
+  return (
+    <>
+      <p>Hello World</p>
+    </>
+  )
+}
+
+export default App;
