2eeab7a6f00c3a9f24cb77202cc6d07bf311c4e0
sayanbose-0000 <bosesayan0000@gmail.com>
2024-02-26 20:52:04 +0530

    added stuffs

--- web/project/bolgme/backend/index.js
+++ web/project/bolgme/backend/index.js
@@ -1,37 +1,44 @@
 import express from 'express'
 import cors from 'cors';
+import mongoose from 'mongoose';
+import userModel from './models/Users.js';
 
 const app = express();
 app.use(cors());
 
 const port = 3000;
 
-app.use(express.json()) // middleware json parser
+app.use(express.json()); // middleware json parser
 
-app.post('/signup', (req, res) => {
+mongoose.connect('mongodb+srv://bosesayan0000:5DTP2SrOm0KuC3A1@cluster0.lb9dlo1.mongodb.net/');
+
+app.post('/signup', async(req, res) => {
   const { userName, userEmail, userPassword } = req.body;
-  res.send(
-    {
-      reqData: {
-        userName,
-        userEmail,
-        userPassword
-      }
-    }
-  );
+
+  try {
+    const userDoc = await userModel.create({
+      userName,
+      userEmail,
+      userPassword
+    })
+
+    res.json(userDoc);
+  } catch (e) {
+    res.status(404).json(e);
+  }
 })
 
-app.post('/login', (req, res) => {
-  const { userName, userEmail, userPassword } = req.body;
-  res.send(
-    {
-      reqData: {
-        userEmail,
-        userPassword
-      }
-    }
-  );
-})
+// app.post('/login', (req, res) => {
+//   const { userName, userEmail, userPassword } = req.body;
+//   res.send(
+//     {
+//       reqData: {
+//         userEmail,
+//         userPassword
+//       }
+//     }
+//   );
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
---
+++ web/project/bolgme/backend/models/Users.js
@@ -0,0 +1,21 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    userEmail: {
+        type: String,
+        required: true,
+    },
+    userPassword: {
+        type: String,
+        required: true,
+    }
+})
+
+const userModel = model('User', userSchema);
+
+export default userModel;
