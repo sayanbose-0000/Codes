82c2d4b20716d3a53bd8ed8572899bb990439d99
sayanbose-0000 <bosesayan0000@gmail.com>
2024-04-25 21:54:42 +0530

    added stuffs

---
+++ dsa/algorithms/mat_chain_mul/mat.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+#define MAX 5
+
+int input[MAX] = {1, 2, 3, 4, 3};
+int m[MAX][MAX];
+int s[MAX][MAX];
+
+void chainMul(int input[], int n) {
+  
+}
+
+int main () {
+  chainMul();
+}
